1c2d9614f8a85ff36567f858835158b9fd38956d
Londiwehlatshwayo <Londiwehlatshwayo>
2022-09-14 17:00:13 -0700

    my 1st commit

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,37 @@
+
+#include &lt;stdio.h&gt;
+
+/**
+ *
+ * * main - causes an infinite loop
+ *
+ * * Return: 0
+ *
+ * */
+
+int main(void)
+
+{
+
+	int i;
+
+	printf(&quot;Infinite loop incoming :(\n&quot;);
+	
+	i = 0;
+/*
+ *
+ * * while (i &lt; 10)
+ *
+ * * {
+ *
+ * * putchar(i);
+ *
+ * * }
+ *
+ * */
+
+	printf(&quot;Infinite loop avoided! \\o/\n&quot;);
+
+	return (0);
+
+}
